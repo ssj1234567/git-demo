f85c96b0d6798745f9f0ac8ffdb607470a4157f0
ssj <ssj@learning.com>
2023-02-18 17:06:09 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/boot/controller/TestGitController.java
+++ src/main/java/com/atguigu/boot/controller/TestGitController.java
@@ -10,6 +10,7 @@ public class TestGitController {
         System.out.println("代码修改了......");
         System.out.println("第三个版本.......");
         System.out.println("hot-fix修改了代码!!!");
+        System.out.println("hot-fix修改了代码aaa!!!");
         return "hello,world!!!";
     }
 }
